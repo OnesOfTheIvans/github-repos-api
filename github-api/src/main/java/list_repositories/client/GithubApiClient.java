b1eb32355f7e2c91b6afe2be91a7ebc4eec53ef8
ooti <val125419@gmail.com>
2025-08-18 14:25:45 +0200

    Added client for git API

--- github-api/src/main/java/list_repositories/client/GithubApiClient.java
+++ github-api/src/main/java/list_repositories/client/GithubApiClient.java
@@ -1,4 +1,44 @@
 package list_repositories.client;
 
+import list_repositories.entities.GithubBranch;
+import list_repositories.entities.GithubRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Component
 public class GithubApiClient {
+    private final RestClient restClient;
+
+    public GithubApiClient(@Value("${github.api.url}") String apiUrl,
+                           @Value("${github.token}") String token) {
+        this.restClient = RestClient.builder()
+                .baseUrl(apiUrl)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "token " + token)
+                .build();
+    }
+
+    public List<GithubRepository> getRepositories(String username) {
+        //TODO try catch
+        String userRepositoriesUrl = "/users/{username}/repos";
+        return restClient.get()
+                .uri(userRepositoriesUrl, username)
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<GithubRepository>>() {
+                });
+    }
+
+    public List<GithubBranch> getBranches(String owner, String repo) {
+        //TODO try catch
+        String repositoryBranchesUrl = "/repos/{owner}/{repo}/branches";
+        return restClient.get()
+                .uri(repositoryBranchesUrl, owner, repo)
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<GithubBranch>>() {
+                });
+    }
 }
